a48bb59b6a4ef0fc022b04e6615aa72623a7746e
IvanInform <38457384+IvanInform@users.noreply.github.com>
2018-04-30 20:49:58 +0300

    Add files via upload

---
+++ DataBaseFirstEntity Framework/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace libraryProjectEntity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnFill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(120, 34);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(673, 20);
+            this.tbFind.TabIndex = 0;
+            this.tbFind.TextChanged += new System.EventHandler(this.tbFind_TextChanged);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(468, 83);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(325, 23);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Search";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(400, 300);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(82, 83);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(275, 23);
+            this.btnFill.TabIndex = 3;
+            this.btnFill.Text = "Fill Tables";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 502);
+            this.Controls.Add(this.btnFill);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.tbFind);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnFill;
+    }
+}
+
---
+++ DataBaseFirstEntity Framework/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace libraryProjectEntity
+{
+    public partial class Form1 : Form
+    {
+        string findauthor;
+        List<Book> listofBooks = new List<Book>();
+        public Form1()
+        {
+            InitializeComponent();
+            btnFind.Enabled = false;
+
+        }
+        #region 
+        //private void fillTables() {
+        //    using (LibraryEntities db = new LibraryEntities())
+        //    {
+                
+        //        //Author a1 = new Author() { FirstName = "Sasha", LastName = "Petrov" };
+        //        //Author a2 = new Author() { FirstName = "Masha", LastName = "Petrova" };
+        //        //Author a3 = new Author() { FirstName = "Egor", LastName = "Pushkin" };
+        //        //Author a4 = new Author() { FirstName = "Grisha", LastName = "Mushin" };
+        //        //db.Authors.AddRange(new Author[] { a1, a2, a3, a4 });
+        //        //db.SaveChanges();
+        //        Book book1 = new Book() { Title = "Book1", Pages = 100, IdAuthor = 1, Publisher = new Publisher() {Address="Street1, Zaporozhye", PublisherName="Publisher1" },
+        //            Price = 300 };
+        //        Book book2 = new Book()
+        //        {
+        //            Title = "Book2",
+        //            Pages = 100,
+        //            IdAuthor = 2,
+        //            Publisher = new Publisher() { Address = "Street2, Kyiv", PublisherName = "Publisher1" },
+        //            Price = 300
+        //        };
+        //        Book book3 = new Book()
+        //        {
+        //            Title = "Book1",
+        //            Pages = 500,
+        //            IdAuthor = 3,
+        //            Publisher = new Publisher() { Address = "Street2, Kharkiv", PublisherName = "Publisher1" },
+        //            Price = 300
+        //        };
+        //        Book book4 = new Book()
+        //        {
+        //            Title = "Book1",
+        //            Pages = 700,
+        //            IdAuthor = 1,
+        //            Publisher = new Publisher() { Address = "Street1, Zaporozhye", PublisherName = "Publisher1" },
+        //            Price = 300
+        //        };
+        //        db.Books.AddRange(new Book[] { book1, book2, book3, book4 });
+        //        db.SaveChanges();
+
+        //    }
+
+        //}
+        #endregion
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            findauthor = tbFind.Text;
+            using (LibraryEntities context = new LibraryEntities())
+            {
+                context.Configuration.LazyLoadingEnabled = false;
+                var author = (from b in context.Authors
+                              where (b.LastName.StartsWith(findauthor))
+                              select b).ToList();
+                foreach (var item in author)
+                {
+                    context.Entry(item).Collection(a => a.Books).Load();
+                    listofBooks.AddRange(item.Books);
+                }
+                dataGridView1.DataSource = listofBooks;
+
+
+            }
+        }
+
+        private void tbFind_TextChanged(object sender, EventArgs e)
+        {
+            if (tbFind.Text.Length > 3) {
+
+                btnFind.Enabled = true;
+            }
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            fillTables();
+        }
+    }
+}
